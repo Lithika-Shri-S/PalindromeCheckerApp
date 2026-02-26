c533f70821cc1620a589084540ca51e9db7ae1aa
Lithika-Shri-S <lithikashri1107@gmail.com>
2026-02-26 09:37:30 +0530

    UC7: Deque-Based Optimized Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,25 +1,23 @@
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
-
 
 public class PalindromeCheckerApp {
-    public static boolean isPalindromeUsingQueueAndStack(String input) {
+    public static boolean isPalindromeUsingDeque(String input) {
 
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> deque = new LinkedList<>();
 
-        // Insert characters into both structures
+        // Insert all characters into deque
         for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            queue.add(ch);   // Enqueue (FIFO)
-            stack.push(ch);  // Push (LIFO)
+            deque.addLast(input.charAt(i));
         }
 
-        // Compare dequeue and pop results
-        while (!queue.isEmpty()) {
-            if (queue.remove() != stack.pop()) {
+        // Compare front and rear elements
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
                 return false;
             }
         }
@@ -34,7 +32,7 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        if (isPalindromeUsingQueueAndStack(input)) {
+        if (isPalindromeUsingDeque(input)) {
             System.out.println("The given string is a palindrome.");
         } else {
             System.out.println("The given string is NOT a palindrome.");
